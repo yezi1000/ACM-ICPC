42497acac8a0e4ce4fd2cbf73e5190ec40f2d431
yezi1000 <30385445+yezi1000@users.noreply.github.com>
2018-05-30 19:17:25 +0800

    1

---
+++ other/c.cpp
@@ -0,0 +1,60 @@
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<set>
+#include<stack>
+#include<map>
+#include<cstring>
+#define eps 1e-8
+#define pi acos(-1)
+#define mp make_pair
+#define PH push
+#define pb push_back
+#define PII pair<int,>
+#define VI vector<int>
+#define fi first
+#define se second
+#define N(a,b) a=N(a,b)
+#define Min(a,b) a=min(a,b)
+#define sqr(x) ((x)*(x))
+#define CL(a) memset(a,0,sizeof a)
+#define fr(i,n) for(int i=1;i<=n;i++)
+#define FR(i,S,n) for(int i=S;i<=n;i++)
+#define dr(i,n) for(int i=n;i;i--)
+#define DR(i,n,S) for(int i=n;i>=S;i--)
+#define SZ(x) ((int)(x).size())
+#define all(n) n.begin(),n.end()
+#define rd(x) scanf("%d",&x)
+#define pr(x) printf("%d",x)
+#define prn(x) printf("%d\n",x)
+typedef long long ll;
+ll expow(ll a,ll b,ll p) {ll v=1; for (; b; b>>=1,a=a*a%p)if (b&1)v=v*a%p; return v;}
+ll inv(ll a,ll p) {return expow(a,p-2,p);}
+using namespace std;
+const int N=100000+10;
+int co[3033],va[3033];
+int dp[4][3033];
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("D:\\GitHub\\ACM-ICPC\\other\\in.txt","r",stdin);
+    #endif
+    int ans=990000000;
+    int n;rd(n);
+    for(int j=1;j<=n;j++) rd(co[j]);
+    for(int j=1;j<=n;j++) rd(va[j]);
+    for(int i=1;i<=n;i++){
+        dp[1][i]=va[i];
+        for(int j=2;j<=3;j++){
+            dp[j][i]=990000000;
+            for(int k=1;k<i;k++){
+                if(co[k]<co[i]) dp[j][i]=min(dp[j][i],dp[j-1][k]+va[i]);
+            }
+        }
+    }
+    for(int j=1;j<=n;j++) ans=min(ans,dp[3][j]);
+    if(ans==990000000) cout<<-1;
+    else cout<<ans;
+}
